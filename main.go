9cb9664d69fa191554da1b8d9b909990104ed47c
Igor Vinicius <viniciusi@ryanair.com>
2020-03-24 00:40:05 +0100

    adding faq and not found pages

--- main.go
+++ main.go
@@ -16,11 +16,22 @@ func contact(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
+func faq(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Frequently Asked Questions</h1><p>Here is a list of questions our users commonly ask.</p>")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Sorry, page not found.</h1>")
+}
 
 func main(){
-
-	r := mux.NewRouter()
+	r := mux.NewRouter()	
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", contact)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	http.ListenAndServe(":3000", r)
 }
