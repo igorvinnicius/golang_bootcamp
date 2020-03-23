cbb4b9434e7b132ca2b3243cda50078feec5338c
Igor Vinicius <viniciusi@ryanair.com>
2020-03-23 17:21:16 +0100

    addin a ServeMux

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-	} else if r.URL.Path == "/contact"{
+	} else if r.URL.Path == "/contact" || r.URL.Path == "/contact/" {
 		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
@@ -19,6 +19,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 }
 
 func main(){
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", mux)
 }
