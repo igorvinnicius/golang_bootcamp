320fe3af4127628d4dc71af654881e5c576c208d
Igor Vinicius <viniciusi@ryanair.com>
2020-03-23 15:19:33 +0100

    Adding a 404 page

--- main.go
+++ main.go
@@ -11,8 +11,11 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 	} else if r.URL.Path == "/contact"{
-		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
-	}	
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We could no find the page you were looking for :(</h1><p>Please email us if you keep being sent to an invalid page.</p>")
+	}
 }
 
 func main(){
