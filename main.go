352c67102ac576bb04751639a644a63240aaa494
Igor Vinicius <igorvinnicius@gmail.com>
2020-03-26 01:19:15 +0100

    implemented home template view

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import(
 	"fmt"
 	"net/http"
+	"html/template"
 	"github.com/gorilla/mux"
 )
 
+var homeTemplate *template.Template
+
 func home(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	if err := homeTemplate.Execute(w, nil); err != nil{
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request){
@@ -28,6 +33,13 @@ func notFound(w http.ResponseWriter, r *http.Request){
 }
 
 func main(){
+	
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
 	r := mux.NewRouter()	
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
