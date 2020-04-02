b630acd82dff074620eb50c9ffbb9110ef42c86a
Igor Vinicius <igorvinnicius@gmail.com>
2020-04-02 15:25:00 +0200

    creating records and logging with GORM

--- exp/main.go
+++ exp/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
+	"strings"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
@@ -18,6 +21,7 @@ type User struct {
 	gorm.Model
 	Name string
 	Email string `gorm:"not null;unique_index"`
+	Color string
 }
 
 func main() {
@@ -29,6 +33,44 @@ func main() {
 
 	defer db.Close()
 
+	db.LogMode(true)
 	db.AutoMigrate(&User{})
+
+	name, email, color := getInfo()
+
+	u := User{
+		Name : name,
+		Email : email,
+		Color: color,
+	}
+
+	if err = db.Create(&u).Error; err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%v\n", u)
+}
+
+func getInfo() (name, email, color string) {
+	
+	reader := bufio.NewReader(os.Stdin)
+	
+	fmt.Println("What's your name?")
+	
+	name, _ = reader.ReadString('\n')
+
+	fmt.Println("What's your email address?")
+	
+	email, _ = reader.ReadString('\n')
+
+	fmt.Println("What's your favorit color?")
+	
+	color, _ = reader.ReadString('\n')
+	
+	name = strings.TrimSpace(name)
+	email = strings.TrimSpace(email)
+	color = strings.TrimSpace(color)
+	
+	return name, email, color
 }
 
