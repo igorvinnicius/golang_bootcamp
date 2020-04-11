d2c69c86a186b66dc99c3d0b079843a39d84f1ff
Igor Vinicius <igorvinnicius@gmail.com>
2020-04-11 14:56:26 +0200

    implementing remember token normalizer

--- models/user.go
+++ models/user.go
@@ -111,10 +111,6 @@ type userValidator struct {
 
 func (uv *userValidator) Create(user *User) error {
 
-	if err := runUserValFuncs(user, uv.bcryptPassword); err != nil {
-		return err
-	}
-
 	if user.Remember == "" {
 		
 		token, err := rand.RememberToken()
@@ -125,20 +121,18 @@ func (uv *userValidator) Create(user *User) error {
 		user.Remember = token		
 	}
 
-	user.RememberHash = uv.hmac.Hash(user.Remember)
+	if err := runUserValFuncs(user, uv.bcryptPassword, uv.hmacRemember); err != nil {
+		return err
+	}		
 
 	return uv.UserDB.Create(user)
 }
 
 func (uv *userValidator) Update(user *User) error {
 
-	if err := runUserValFuncs(user, uv.bcryptPassword); err != nil {
+	if err := runUserValFuncs(user, uv.bcryptPassword, uv.hmacemember); err != nil {
 		return err
-	}
-
-	if user.Remember != "" {
-		user.RememberHash = uv.hmac.Hash(user.Remember)
-	}
+	}	
 
 	return uv.UserDB.Update(user)
 }
@@ -172,9 +166,28 @@ func (uv *userValidator) bcryptPassword(user *User) error {
 	return nil
 }
 
+func (uv *userValidator) hmacRemember(user *User) error {
+
+	if user.Remember == "" {
+		return nil
+	}
+
+	user.RememberHash = uv.hmac.Hash(user.Remember)
+
+	return nil
+}
+
 func (uv *userValidator) ByRemember(token string) (*User, error) {
-	rememberHash := uv.hmac.Hash(token)	
-	return uv.UserDB.ByRemember(rememberHash)
+	
+	user := User{
+		Remember: token,
+	}
+
+	if err := runUserValFuncs(&user, uv.hmacRemember); err != nil {
+		return nil, err
+	}
+
+	return uv.UserDB.ByRemember(user.RememberHash)
 }
 
 var _ UserDB = &userGorm{}
