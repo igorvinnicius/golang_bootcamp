6a09a4bd772cc658c9602b33f53dfb2ed58a173b
Igor Vinicius <igorvinnicius@gmail.com>
2020-04-13 17:32:31 +0200

    validating and normalizing remember tokens

--- models/user.go
+++ models/user.go
@@ -22,6 +22,8 @@ var(
 	ErrEmailTaken = errors.New("models: email address is already taken")
 	ErrPasswordTooShort = errors.New("models: password must be at least 8 ccarachters long")
 	ErrPasswordRequired =  errors.New("models: password is required")
+	ErrRememberTooShort = errors.New("models: remember token must be at least 32 bytes")
+	ErrRememberRequired = errors.New("models: remember token is required")
 )
 
 const userPwPepper = "secret-random-string"
@@ -131,8 +133,10 @@ func (uv *userValidator) Create(user *User) error {
 		uv.passwordMinLength,
 		uv.bcryptPassword,
 		uv.passwordHashRequired,
-		uv.setRememberIfUnset, 
+		uv.setRememberIfUnset,
+		uv.rememberMinBytes,
 		uv.hmacRemember,
+		uv.rememberHashRequired,
 		uv.normalizeEmail,
 		uv.requireEmail,
 		uv.emailFormat,
@@ -151,7 +155,9 @@ func (uv *userValidator) Update(user *User) error {
 		uv.passwordMinLength,
 		uv.bcryptPassword,
 		uv.passwordHashRequired,
+		uv.rememberMinBytes,
 		uv.hmacRemember,
+		uv.rememberHashRequired,
 		uv.normalizeEmail,
 		uv.requireEmail,
 		uv.emailFormat,
@@ -220,6 +226,33 @@ func (uv *userValidator) setRememberIfUnset(user *User) error {
 	}
 
 	user.Remember = token			
+
+	return nil
+}
+
+func (uv *userValidator) rememberMinBytes(user *User) error { 
+
+	if user.Remember == "" {
+		return nil
+	}
+
+	n, err := rand.NBytes(user.Remember)
+
+	if err != nil {
+		return err
+	}
+
+	if n < 32 {
+		return ErrRememberTooShort
+	}
+
+	return nil
+}
+
+func (uv *userValidator) rememberHashRequired(user *User) error { 
+	if user.RememberHash == "" {
+		return ErrRememberRequired
+	}
 
 	return nil
 }
