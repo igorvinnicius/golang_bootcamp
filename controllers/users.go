1fda409a23eae90b91f252665f180b4593fba994
Igor Vinicius <igorvinnicius@gmail.com>
2020-04-13 19:26:21 +0200

    creating dynamic alert

--- controllers/users.go
+++ controllers/users.go
@@ -34,7 +34,18 @@ type Users struct{
 }
 
 func (u *Users) New(w http.ResponseWriter, r *http.Request){
-	if err := u.NewView.Render(w, nil); err != nil {
+	
+	type Alert struct {
+		Level string
+		Message string
+	}
+	
+	a := Alert {
+		Level: "success",
+		Message: "Successfully rendered a dynamic alert!",
+	}
+
+	if err := u.NewView.Render(w, a); err != nil {
 		panic(err)
 	}
 }
