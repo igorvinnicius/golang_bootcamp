560e554a6382de63cede299c1856ec8a069ca319
Igor Vinicius <igorvinnicius@gmail.com>
2020-04-15 01:54:47 +0200

    sharing a GORM connection and the GalleryService

--- models/galleries.go
+++ models/galleries.go
@@ -7,3 +7,17 @@ type Gallery struct {
 	UserId uint `gorm:"not null;index"`
 	Title string `gorm:"not null"`
 }
+
+type GalleryService interface {
+	GalleryDB
+}
+
+type GalleryDB interface {
+
+	Create(gallery *Gallery) error
+
+}
+
+type galleryGorm struct {
+	db *gorm.DB
+}
